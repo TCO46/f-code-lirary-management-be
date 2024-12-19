246d3bfacd75a019ff7400eb7738abb662d925fb
tiendat2k6 <104376586+tiendat2k6@users.noreply.github.com>
2024-12-19 19:02:59 +0700

    kết nối frontend với backend cho login

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
+const cors = require('cors');
+
+// Enable CORS for all routes
+
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 const { Database } = require("./config/index");
 const { route } = require("./routes/index");
+// const loginRoute = require('./routes/api/auth/login');
+// const AuthController = require("./app/controllers/AuthController");
+const authRouter = require("./routes/api/auth");
+
 
 const UserModel = require("./app/models/User");
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 // connect to database
 const db = new Database();
@@ -20,6 +30,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(morgan("dev"));
+// app.use("/api/auth/login", loginRoute);
+// app.use(express.json())
+app.use("/api/auth", authRouter); 
+
 
 // route
 app.get("/", (req, res, next) => {
@@ -31,6 +45,7 @@ app.get("/", (req, res, next) => {
 			})
 			.catch(next);
 });
+
 route(app);
 
 // app listen
